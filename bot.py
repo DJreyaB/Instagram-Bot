802ef9cbda66eb0cd43d267cba351abc8b435fc3
djreyab <djreya.boyd@gmail.com>
2020-07-24 02:02:33 -0400

    bare bones of the Instagram Bot laying out the functions

---
+++ bot.py
@@ -0,0 +1,21 @@
+#packages
+
+#Insure user is logged in to instagram
+def login_verification():
+    pass;
+
+#Search for keyword 
+def hashtag():
+    pass;
+
+#Choose random user to comment under with a recent date and minimum amount of engagement
+def pick_user():
+    pass;
+
+#Verify this post hasn't been posted under before by searching comments for bot names
+def dup_user():
+    pass;
+
+#Post one of the approved comments 
+def comment():
+    pass;
