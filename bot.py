253bc4b7b2456a486d2261bacf8e42536dcbead1
djreyab <djreya.boyd@gmail.com>
2020-07-25 01:26:31 -0400

    Removed errors from previous files. Unnecessary semicolon after pass within functions.

--- bot.py
+++ bot.py
@@ -6,29 +6,20 @@
 def login_verification():
     #find discrete way to login to instagram
     #pull username and password from a dictionary?
-    pass;
-
-#finds whats trending in a specific location
-#create a default zip code to find whats trendy in general
-def trendy_here():
-    pass;
-
-#Search for keyword 
-def hashtag():
-    pass;
+    pass
 
 #Choose random user to comment under with a recent date and minimum amount of engagement
 def pick_user():
-    pass;
+    pass
 
 #Verify this post hasn't been posted under before by searching comments for bot names
 def dup_user():
-    pass;
+    pass
 
 #Post one of the approved comments 
 def comment():
-    pass;
+    pass
 
 #List of users to potentially follow to increase following.
 def follow_list():
-    pass;
+    pass
--- trendy.py
+++ trendy.py
@@ -3,12 +3,12 @@
 #finds whats trending in a specific location
 #create a default zip code to find whats trendy in general
 def trendy_here():
-    pass;
+    pass
 
 #Search for keyword 
 def hashtag():
-    pass;
+    pass
 
 #Creates a list of all the trendy hashtags with their analytics
 def trend_list():
-    pass;
+    pass
