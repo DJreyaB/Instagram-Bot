450c0d42b2b26bab1ae4a62ccf0fd1951a5059be
djreyab <djreya.boyd@gmail.com>
2020-07-24 22:43:52 -0400

    Added new file to separate trends analysis from bot file

---
+++ trendy.py
@@ -0,0 +1,14 @@
+
+
+#finds whats trending in a specific location
+#create a default zip code to find whats trendy in general
+def trendy_here():
+    pass;
+
+#Search for keyword 
+def hashtag():
+    pass;
+
+#Creates a list of all the trendy hashtags with their analytics
+def trend_list():
+    pass;
